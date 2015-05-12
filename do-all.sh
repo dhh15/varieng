4b007de6e92f8483c54f8ee282754582cbab8649
Jukka Suomela <jukka.suomela@iki.fi>
2015-05-12 20:31:10 +0300

    More plots.

--- do-all.sh
+++ do-all.sh
@@ -3,13 +3,13 @@
 # Assumption:
 # ../dhh15 = input data from https://bitbucket.org/suomela/dhh15
 
-log="log/log.txt"
-
 mkdir -p plot log || exit 1
-rm -f $log
-in-time/in-time plot/scotl-engl.pdf '\<scotl' '\<engl' | tee -a $log
-in-time/in-time plot/scotl-scotx.pdf '\<scotl' '\<scot[^l]' | tee -a $log
-in-time/in-time plot/union.pdf '\<union' | tee -a $log
-in-time/in-time plot/kingd.pdf '\<kingd' | tee -a $log
-in-time/in-time plot/brit.pdf '\<brit' | tee -a $log
-in-time/in-time plot/pretender.pdf '\<pretender\>' | tee -a $log
+in-time/in-time plot/scotl-engl.pdf '\<scotl' '\<engl' &
+in-time/in-time plot/scotl-scotx.pdf '\<scotl' '\<scot[^l]' &
+in-time/in-time plot/union.pdf '\<union' &
+in-time/in-time plot/kingd.pdf '\<kingd' &
+in-time/in-time plot/brit.pdf '\<brit' &
+in-time/in-time plot/pretender.pdf '\<pretender\>' &
+in-time/in-time -sex -region plot/scotl-engl-sex-region.pdf '\<scotl' '\<engl' &
+in-time/in-time -sex -region plot/union-sex-region.pdf '\<union' &
+wait
