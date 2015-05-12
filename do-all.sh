bc39df786f96e34a252ff94c1818b6abb2deeb1c
Jukka Suomela <jukka.suomela@iki.fi>
2015-05-12 20:56:22 +0300

    New plots.

--- do-all.sh
+++ do-all.sh
@@ -3,13 +3,22 @@
 # Assumption:
 # ../dhh15 = input data from https://bitbucket.org/suomela/dhh15
 
-mkdir -p plot || exit 1
-bin/in-time plot/scotl-engl.pdf '\<scotl' '\<engl' &
-bin/in-time plot/scotl-scotx.pdf '\<scotl' '\<scot[^l]' &
-bin/in-time plot/union.pdf '\<union' &
-bin/in-time plot/kingd.pdf '\<kingd' &
-bin/in-time plot/brit.pdf '\<brit' &
-bin/in-time plot/pretender.pdf '\<pretender\>' &
-bin/in-time -sex -region plot/scotl-engl-sex-region.pdf '\<scotl' '\<engl' &
-bin/in-time -sex -region plot/union-sex-region.pdf '\<union' &
+mkdir -p plot-scotland plot-language || exit 1
+
+bin/in-time plot-scotland/scotl-engl.pdf '\<scotl' '\<engl' &
+bin/in-time plot-scotland/scotl-scotx.pdf '\<scotl' '\<scot[^l]' &
+bin/in-time plot-scotland/union.pdf '\<union' &
+bin/in-time plot-scotland/kingd.pdf '\<kingd' &
+bin/in-time plot-scotland/brit.pdf '\<brit' &
+bin/in-time plot-scotland/pretender.pdf '\<pretender\>' &
+bin/in-time -region plot-scotland/scotl-engl-region.pdf '\<scotl' '\<engl' &
+bin/in-time -region plot-scotland/union-region.pdf '\<union' &
+bin/in-time -sex plot-scotland/scotl-engl-sex.pdf '\<scotl' '\<engl' &
+bin/in-time -sex plot-scotland/union-sex.pdf '\<union' &
+
+bin/in-time plot-language/hath-has.pdf '\<hath\>' '\<has\>' &
+bin/in-time -region plot-language/hath-has-region.pdf '\<hath\>' '\<has\>' &
+bin/in-time -sex plot-language/hath-has-sex.pdf '\<hath\>' '\<has\>' &
+bin/in-time -sex -region plot-language/hath-has-sex-region.pdf '\<hath\>' '\<has\>' &
+
 wait
