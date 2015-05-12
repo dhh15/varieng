764d333bb9fa471682490b888f8f89e9ea9e41dd
Jukka Suomela <jukka.suomela@iki.fi>
2015-05-12 16:28:47 +0300

    Logging, clean-up of output.

--- do-all.sh
+++ do-all.sh
@@ -3,6 +3,9 @@
 # Assumption:
 # ../dhh15 = input data from https://bitbucket.org/suomela/dhh15
 
-mkdir -p plot || exit 1
-in-time/in-time plot/scotl-engl.pdf '\<scotl' '\<engl' || exit 1
-in-time/in-time plot/scotl-scotx.pdf '\<scotl' '\<scot[^l]' || exit 1
+log="log/log.txt"
+
+mkdir -p plot log || exit 1
+rm -f $log
+in-time/in-time plot/scotl-engl.pdf '\<scotl' '\<engl' | tee -a $log
+in-time/in-time plot/scotl-scotx.pdf '\<scotl' '\<scot[^l]' | tee -a $log
