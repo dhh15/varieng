1a36115caad9009782703b66647fe31e575e1637
Joseph Flanagan <joseph.flanagan@helsinki.fi>
2015-05-12 23:55:44 +0300

    Write script to export clean text files

---
+++ R/export-clean-ceec.R
@@ -0,0 +1,52 @@
+#########################################################################################
+# File:  export-clean-ceec.R
+# Date:  2015-05-12  
+# Author: Joseph Flanagan
+# email:   joseph.flanagan@helsinki.fi
+# Purpose: Export text files from the CEEC corpus that have been cleaned of metadata and punctuation
+
+#########################################################################################
+
+library("NLP")
+library("openNLP")
+library("magrittr")
+
+# Get files from subdirectory (warning: imports all files in the subdirectory)
+
+filenames <- Sys.glob("data/ceec-normalised/*.txt")
+
+
+# Read in files
+texts <- filenames %>%
+    lapply(readLines) %>%
+    lapply(paste0, collapse = " ") %>%
+    lapply(as.String) 
+
+
+# Remove metadata, numbers, and punctuation
+texts <- lapply(texts, function(x){
+    x = gsub("\\[\\{(.*?)\\{\\]", "\\1", x)
+    x = gsub("<[^>]+>","", x)
+    x = gsub("\\[.*?\\]", "", x)
+    x = gsub("}\\]", "", x)
+    x = gsub("\\d","", x) # get rid of numbers
+    x = gsub("[^[:alnum:][:space:]']", "", x) # get rid of punctuation except for apostrophes
+    x
+})
+
+
+# Name files according to file name
+names(texts) <- basename(filenames)
+
+
+# Convert to base r string (since NLP strings won't export)
+texts <- lapply(texts, function(x){
+    x = toString(x)
+    x
+})
+
+
+# Export to clean-data subdirectory
+for (i in 1:length(texts)) {
+    write.csv(texts[i], file=paste0("output/cleaned-data/", names(texts)[i]))
+}
