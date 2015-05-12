24c883c2a1e3836df93559d7359d04e31d860314
Jukka Suomela <jukka.suomela@iki.fi>
2015-05-12 20:40:57 +0300

    Rename directory.

--- do-all.sh
+++ do-all.sh
@@ -4,12 +4,12 @@
 # ../dhh15 = input data from https://bitbucket.org/suomela/dhh15
 
 mkdir -p plot || exit 1
-in-time/in-time plot/scotl-engl.pdf '\<scotl' '\<engl' &
-in-time/in-time plot/scotl-scotx.pdf '\<scotl' '\<scot[^l]' &
-in-time/in-time plot/union.pdf '\<union' &
-in-time/in-time plot/kingd.pdf '\<kingd' &
-in-time/in-time plot/brit.pdf '\<brit' &
-in-time/in-time plot/pretender.pdf '\<pretender\>' &
-in-time/in-time -sex -region plot/scotl-engl-sex-region.pdf '\<scotl' '\<engl' &
-in-time/in-time -sex -region plot/union-sex-region.pdf '\<union' &
+bin/in-time plot/scotl-engl.pdf '\<scotl' '\<engl' &
+bin/in-time plot/scotl-scotx.pdf '\<scotl' '\<scot[^l]' &
+bin/in-time plot/union.pdf '\<union' &
+bin/in-time plot/kingd.pdf '\<kingd' &
+bin/in-time plot/brit.pdf '\<brit' &
+bin/in-time plot/pretender.pdf '\<pretender\>' &
+bin/in-time -sex -region plot/scotl-engl-sex-region.pdf '\<scotl' '\<engl' &
+bin/in-time -sex -region plot/union-sex-region.pdf '\<union' &
 wait
