be454f120ea081796658847765ea2c4435f1d83d
Jukka Suomela <jukka.suomela@iki.fi>
2015-05-12 20:32:04 +0300

    log no longer needed.

--- do-all.sh
+++ do-all.sh
@@ -3,7 +3,7 @@
 # Assumption:
 # ../dhh15 = input data from https://bitbucket.org/suomela/dhh15
 
-mkdir -p plot log || exit 1
+mkdir -p plot || exit 1
 in-time/in-time plot/scotl-engl.pdf '\<scotl' '\<engl' &
 in-time/in-time plot/scotl-scotx.pdf '\<scotl' '\<scot[^l]' &
 in-time/in-time plot/union.pdf '\<union' &
