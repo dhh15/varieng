20383c7f3eaaf0a20080872c435f9bdfff2e392c
Jukka Suomela <jukka.suomela@iki.fi>
2015-05-12 16:48:27 +0300

    More.

--- do-all.sh
+++ do-all.sh
@@ -9,3 +9,5 @@ mkdir -p plot log || exit 1
 rm -f $log
 in-time/in-time plot/scotl-engl.pdf '\<scotl' '\<engl' | tee -a $log
 in-time/in-time plot/scotl-scotx.pdf '\<scotl' '\<scot[^l]' | tee -a $log
+in-time/in-time plot/union.pdf '\<union' | tee -a $log
+in-time/in-time plot/kingd.pdf '\<kingd' | tee -a $log
