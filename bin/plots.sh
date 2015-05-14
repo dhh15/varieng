748403d6267f4ecb39d2ef65a28d413da956d8d0
Jukka Suomela <jukka.suomela@iki.fi>
2015-05-14 14:33:31 +0300

    Human-readable labels.

--- bin/plots.sh
+++ bin/plots.sh
@@ -7,32 +7,32 @@ mkdir -p plot-scotland plot-language || exit 1
 
 scot='\<scot(\>|[^t]\>|[a-z][a-z])'
 
-bin/in-time                 plot-scotland/scot.pdf              "$scot" &
-bin/in-time -sex            plot-scotland/scot-sex.pdf          "$scot" &
-bin/in-time -region         plot-scotland/scot-region.pdf       "$scot" &
-bin/in-time -gentry         plot-scotland/scot-gentry.pdf       "$scot" &
-bin/in-time -sex -region    plot-scotland/scot-sex-region.pdf   "$scot" &
-bin/in-time -sex -gentry    plot-scotland/scot-sex-gentry.pdf   "$scot" &
-bin/in-time -print  -period plot-scotland/scot-period.txt       "$scot" &
+bin/in-time                 plot-scotland/scot.pdf              "$scot" 'scot...' &
+bin/in-time -sex            plot-scotland/scot-sex.pdf          "$scot" 'scot...' &
+bin/in-time -region         plot-scotland/scot-region.pdf       "$scot" 'scot...' &
+bin/in-time -gentry         plot-scotland/scot-gentry.pdf       "$scot" 'scot...' &
+bin/in-time -sex -region    plot-scotland/scot-sex-region.pdf   "$scot" 'scot...' &
+bin/in-time -sex -gentry    plot-scotland/scot-sex-gentry.pdf   "$scot" 'scot...' &
+bin/in-time -print  -period plot-scotland/scot-period.txt       "$scot" 'scot...' &
 
-bin/in-time                 plot-scotland/queen.pdf             '\<queen' &
-bin/in-time -sex            plot-scotland/queen-sex.pdf         '\<queen' &
-bin/in-time -region         plot-scotland/queen-region.pdf      '\<queen' &
-bin/in-time -gentry         plot-scotland/queen-gentry.pdf      '\<queen' &
-bin/in-time -print  -sex    plot-scotland/queen-sex.txt         '\<queen' &
+bin/in-time                 plot-scotland/queen.pdf             '\<queen' 'queen...' &
+bin/in-time -sex            plot-scotland/queen-sex.pdf         '\<queen' 'queen...' &
+bin/in-time -region         plot-scotland/queen-region.pdf      '\<queen' 'queen...' &
+bin/in-time -gentry         plot-scotland/queen-gentry.pdf      '\<queen' 'queen...' &
+bin/in-time -print  -sex    plot-scotland/queen-sex.txt         '\<queen' 'queen...' &
 
-bin/in-time                 plot-scotland/union.pdf             '\<union' &
-bin/in-time -print          plot-scotland/union.txt             '\<union' &
+bin/in-time                 plot-scotland/union.pdf             '\<union' 'union...' &
+bin/in-time -print          plot-scotland/union.txt             '\<union' 'union...' &
 
-bin/in-time                 plot-scotland/engl.pdf              '\<engl' &
-bin/in-time                 plot-scotland/brit.pdf              '\<brit' &
-bin/in-time                 plot-scotland/king.pdf              '\<king' &
-bin/in-time                 plot-scotland/pretender.pdf         '\<pretender\>' &
+bin/in-time                 plot-scotland/engl.pdf              '\<engl' 'engl...' &
+bin/in-time                 plot-scotland/brit.pdf              '\<brit' 'brit...' &
+bin/in-time                 plot-scotland/king.pdf              '\<king' 'king...' &
+bin/in-time                 plot-scotland/pretender.pdf         '\<pretender\>' 'pretender' &
 
-bin/in-time                 plot-language/hath-has.pdf              '\<hath\>' '\<has\>' &
-bin/in-time -sex            plot-language/hath-has-sex.pdf          '\<hath\>' '\<has\>' &
-bin/in-time -region         plot-language/hath-has-region.pdf       '\<hath\>' '\<has\>' &
-bin/in-time -gentry         plot-language/hath-has-gentry.pdf       '\<hath\>' '\<has\>' &
-bin/in-time -sex -region    plot-language/hath-has-sex-region.pdf   '\<hath\>' '\<has\>' &
+bin/in-time                 plot-language/hath-has.pdf              '\<hath\>' 'hath' '\<has\>' 'has' &
+bin/in-time -sex            plot-language/hath-has-sex.pdf          '\<hath\>' 'hath' '\<has\>' 'has' &
+bin/in-time -region         plot-language/hath-has-region.pdf       '\<hath\>' 'hath' '\<has\>' 'has' &
+bin/in-time -gentry         plot-language/hath-has-gentry.pdf       '\<hath\>' 'hath' '\<has\>' 'has' &
+bin/in-time -sex -region    plot-language/hath-has-sex-region.pdf   '\<hath\>' 'hath' '\<has\>' 'has' &
 
 wait
